71142e9ea6a0d78201a098399a1c0731ae7bb69d
ashafaatadhis <skullreaper387@gmail.com>
2023-02-25 14:13:20 +0700

    chore(*): forget to run prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
-  content: [
-    'src/**/*.{js,ts,jsx,tsx}',
-    'node_modules/react-tailwindcss-datepicker/dist/index.esm.js',
-    'node_modules/daisyui/dist/**/*.js',
-    'node_modules/react-daisyui/dist/**/*.js',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  daisyui: {
-    themes: false,
-  },
+    darkMode: "class",
+    content: [
+        "src/**/*.{js,ts,jsx,tsx}",
+        "node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
+        "node_modules/daisyui/dist/**/*.js",
+        "node_modules/react-daisyui/dist/**/*.js"
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: [],
+    daisyui: {
+        themes: false
+    }
 };
